368597bbd022e97c10d472c6e8b4440c2d1da5d8
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-19 03:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const container = document.createElement("div");
+container.className = "container";
+document.body.appendChild(container);
+
+const colors = [
+  "#e74c3c", "#8e44ad", "#3498db",
+  "#e67e22", "#2ecc71", "#f1c40f"
+];
+
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement("div");
+  square.className = "square";
+
+  square.addEventListener("mouseenter", () => {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    square.style.backgroundColor = color;
+  });
+
+  square.addEventListener("mouseleave", () => {
+    square.style.backgroundColor = "#1d1d1d";
+  });
+
+  container.appendChild(square);
+}
